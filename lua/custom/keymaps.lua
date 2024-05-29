1dbfadd8bcb4c0f32af22458b12a0062ef2374ac
caleb-kuss <kusscall@gmail.com>
2024-05-28 20:04:02 -0500

    remove unused keymaps and add map for tabs

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,15 +1,6 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
-keymap.set('n', 'x', '"_x')
-
--- Increment/decrement
-keymap.set('n', '+', '<C-a>')
-keymap.set('n', '-', '<C-x>')
-
--- Select all
-keymap.set('n', '<C-a>', 'gg<S-v>G')
-
 -- Save file and quit
 keymap.set('n', '<Leader>w', ':update<Return>', opts)
 keymap.set('n', '<Leader>q', ':quit<Return>', opts)
@@ -17,6 +8,7 @@ keymap.set('n', '<Leader>Q', ':qa<Return>', opts)
 
 -- Tabs
 keymap.set('n', 'te', ':tabedit')
+keymap.set('n', 'tn', ':tabnew<Return>', opts)
 keymap.set('n', '<tab>', ':tabnext<Return>', opts)
 keymap.set('n', '<s-tab>', ':tabprev<Return>', opts)
 keymap.set('n', 'tw', ':tabclose<Return>', opts)
