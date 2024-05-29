18dfe360b6ae2c8234ff7ab1566bddcbbef4651e
caleb-kuss <kusscall@gmail.com>
2024-05-28 19:51:32 -0500

    chore: stop neotree from opening by default

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -9,7 +9,6 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
   },
