cf73ad0ede5d99c5a711c98cfdd160edf4c75e0f
Caleb Kuss <caleb.kuss@coschedule.com>
2024-05-31 07:12:07 -0500

    chore: add keymaps for epoch and neogit

--- lua/custom/plugins/cowboy_epoch.lua
+++ lua/custom/plugins/cowboy_epoch.lua
@@ -1,3 +1,14 @@
 return {
   'cowboy8625/epoc.nvim',
+  config = function()
+    local wk = require 'which-key'
+    wk.register({
+      E = {
+        name = 'Epoch Time',
+        t = { ':EpochTimeConvertToClipboard<CR>', 'Convert to Clipboard' },
+        c = { ':EpochTimeCurrentToClipboard<CR>', 'Current to Clipboard' },
+        p = { ':EpochTimeConvertAndPopup<CR>', 'Convert and Popup' },
+      },
+    }, { prefix = '<leader>' })
+  end,
 }
