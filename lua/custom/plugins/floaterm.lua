4e90e3db5c2e3436f90035d10eee2c5ab3589f4f
Caleb Kuss <caleb.kuss@coschedule.com>
2024-08-06 10:54:58 -0500

    fix: keymaps for floaterm

--- lua/custom/plugins/floaterm.lua
+++ lua/custom/plugins/floaterm.lua
@@ -8,7 +8,10 @@ return {
     vim.g.floaterm_wintitle = 0
     vim.g.floaterm_autoclose = 1
   end,
-  vim.keymap.set('n', '<c-_>', ':FloatermToggle<CR>', { desc = 'Toggle [F]loaterm' }),
-  vim.keymap.set('i', '<c-_>', ':FloatermToggle<CR>', { desc = 'Toggle [F]loaterm' }),
-  vim.keymap.set('t', '<c-_>', '<c-\\><c-n>:FloatermToggle<CR>', { desc = 'Toggle [F]loaterm' }),
+
+  vim.keymap.set('n', '<c-/>', ':FloatermToggle<CR>', { desc = 'Toggle [F]loaterm' }),
+  vim.keymap.set('t', '<c-/>', '<c-\\><c-n>:FloatermToggle<CR>', { desc = 'Toggle [F]loaterm' }),
+  vim.keymap.set('t', '<Leader>fN', '<c-\\><c-n>:FloatermNew<CR>', { desc = 'New [F]loaterm' }),
+  vim.keymap.set('t', '<Leader>fp', '<c-\\><c-n>:FloatermPrev<CR>', { desc = 'Previous [F]loaterm' }),
+  vim.keymap.set('t', '<Leader>fn', '<c-\\><c-n>:FloatermNext<CR>', { desc = 'Next [F]loaterm' }),
 }
