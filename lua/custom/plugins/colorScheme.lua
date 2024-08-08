3eefe4243b42a9294bf48f73a7a2877c96aa649b
Caleb Kuss <kusscall@gmail.com>
2024-08-08 06:11:20 -0500

    change color profile

--- lua/custom/plugins/colorScheme.lua
+++ lua/custom/plugins/colorScheme.lua
@@ -1,11 +1,11 @@
 return {
-  {
-    'b0o/lavi.nvim',
-    dependencies = { 'rktjmp/lush.nvim' },
-    config = function()
-      vim.cmd [[colorscheme lavi]]
-    end,
-  },
+  -- {
+  --   'b0o/lavi.nvim',
+  --   dependencies = { 'rktjmp/lush.nvim' },
+  --   config = function()
+  --     vim.cmd [[colorscheme lavi]]
+  --   end,
+  -- },
   --  First color scheme.
   -- {
   --   'AlexvZyl/nordic.nvim',
@@ -15,4 +15,16 @@ return {
   --     require('nordic').load()
   --   end,
   -- },
+  {
+    'neanias/everforest-nvim',
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require('everforest').setup {
+        -- Your config here
+      }
+    end,
+  },
 }
